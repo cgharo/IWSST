a36121caa98673f8f97bce7ed4523e71af37e89b
Cristina GONZALEZ HARO <cgonzale@br146-159.ifremer.fr>
2018-03-16 12:32:59 +0100

    Plot available SST snapshots and track days with more than one image per Region

---
+++ available_SST_images.py
@@ -0,0 +1,137 @@
+import matplotlib.pyplot as plt
+import matplotlib.colors as color
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import numpy as np
+import xarray as xr
+#from cmocean import cm
+import os
+import fnmatch as fn
+import pandas as pd
+from glob import iglob
+import matplotlib.dates as mdates
+import datetime
+import locale
+
+locale.setlocale(locale.LC_ALL,'en_US.utf8')
+
+datadir = '/home/cercache/users/cgonzale/data/test'#root path for data
+graphdir = '/home/cercache/users/cgonzale/snapshots/'#root path for snapshots of SST
+
+
+#Parameters that will change to define region and period of interest
+Region = 'Mozamb2'
+
+region_folder = datadir + os.sep + Region + os.sep
+
+#Find available files in the region
+rootdir_glob = region_folder+ '**/*'
+file_list = [f for f in iglob(rootdir_glob, recursive=True) if os.path.isfile(f)]
+
+#check how many available  sensors for the given region
+fsensor = [f.rsplit(os.sep,4)[1] for f in file_list]
+
+sensor = list(set(fsensor))
+
+# Read date from filenames and convert the string into datetime
+date = [f.rsplit(os.sep,1)[1][0:14] for f in file_list]
+format='%Y%m%d%H%M%S'
+date_time = pd.to_datetime(date, format=format)
+
+
+
+
+#Find dates with more than one SST image
+
+data_dict = {'date' : date_time, 'sensor':fsensor,'path':file_list}
+df = pd.DataFrame(data_dict)
+
+df = df.sort_values('date')
+
+#Append new column for day_index
+df['day_index'] = df['date'].apply(lambda x:str(x.year)+str(x.month)+str(x.day))
+df = df[df.day_index.duplicated(keep=False)]
+a = df.groupby('day_index').apply(lambda x: list(x.path))
+list_day_images = a.values
+
+
+#print results per date and path to data file
+data_file_fname = pd.DataFrame([])
+
+for days in range(len(list_day_images)):
+    date = [list_day_images[days][0].rsplit(os.sep,1)[1][0:8]]
+    print('###############')
+    print(date)
+    print('    files:')
+    for f in list_day_images[days]:
+        data_file_fname = data_file_fname.append(pd.DataFrame({'date': date,'path':f}, index=[0]))
+        print(f)
+
+
+#save csv file
+filename = datadir+ os.sep + 'Available_images_LOG' +os.sep + Region+'_available_SST_images.csv'
+data_file_fname.to_csv(filename, sep='\t')
+
+
+#Figure with available data
+#inizialitation of variable to plot
+plot_sensor = np.zeros([len(date_time)])
+
+years = mdates.YearLocator()   # every year
+months = mdates.MonthLocator()  # every month
+days = mdates.DayLocator()  # every day
+yearsFmt = mdates.DateFormatter('%Y')
+
+symb_sensor = ['o','^']
+
+
+locale.setlocale(locale.LC_ALL,'en_US.utf8')
+fig, ax = plt.subplots()
+
+for s in range(len(sensor)):
+    isensor = [True if sensor[s] in i else False for i in file_list]
+    ax.plot(date_time[isensor], plot_sensor[isensor]+s+0.5,symb_sensor[s],label = sensor[s])
+
+
+datemin = datetime.date(date_time.min().year, 1, 1)
+datemax = datetime.date(date_time.date.max().year + 1, 1, 1)
+ax.set_xlim(datemin, datemax)
+ax.set_ylim(0, len(sensor))
+
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%b'))
+ax.xaxis.set_major_locator(months)
+ax.xaxis.set_minor_locator(days)
+ax.xaxis.set_ticks_position('bottom')
+
+
+ax.legend()
+ax2 = ax.twiny()
+
+#offset the twin axes below the host
+ax2.spines["bottom"].set_position(("axes", -0.2))
+
+#turn on the frame for the twin axis, but then hide all
+ax2.set_frame_on(True)
+ax2.patch.set_visible(False)
+#for sp in ax2.spines.intervalues():
+#    sp.set_visible(False)
+ax2.spines["bottom"].set_visible(True)
+
+ax2.xaxis.set_ticks_position('bottom')
+ax2.xaxis.set_label_position('bottom')
+
+# format the ticks
+ax2.xaxis.set_major_locator(years)
+ax2.xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
+ax2.set_xlabel('Time')
+#ax2.xaxis.set_minor_locator(months)
+
+# rotates and right aligns the x labels, and moves the bottom of the
+# axes up to make room for them
+fig.autofmt_xdate()
+
+plt.show(block = False)
+
+
+
+
---
+++ plot_snapshots_SST.py
@@ -0,0 +1,105 @@
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import numpy as np
+import xarray as xr
+from netCDF4 import Dataset
+from cmocean import cm
+import pandas as pd
+import os
+import fnmatch as fn
+from glob import iglob
+import sys
+import argparse
+from utils import *
+
+######################
+def date_to_nth_day(date, format='%Y%m%d'):
+    date = pd.to_datetime(date, format=format)
+    new_year_day = pd.Timestamp(year=date.year, month=1, day=1)
+    return (date - new_year_day).days + 1
+
+def safe_make_folder(i):
+    '''Makes a folder if not present'''
+    try:  
+        os.makedirs(i)
+        print('creating: ', i)
+    except:
+        pass
+
+########################
+
+
+def plot_snapshots_SST(Region , datadir = '/home/cercache/users/cgonzale/data/test' ,graphdir=  '/home/cercache/users/cgonzale/snapshots'):
+  
+
+
+    region_folder = datadir+os.sep + Region+os.sep
+    rootdir_glob = region_folder+ '**/*'
+    #find the path of available data
+    file_list = [f for f in iglob(rootdir_glob, recursive=True) if os.path.isfile(f)]
+
+
+    for file in file_list:  
+        path_graph =  file.rsplit(os.sep,1)[0].split(os.sep,7)[-1]
+        #Create path if it does not exist
+        safe_make_folder(graphdir + os.sep+ path_graph)
+    
+        #may check if the figure already exists before open file and do the plot
+        ncfile=Dataset(file,'r')
+        lat = ncfile.variables['lat'][:]
+        lon = ncfile.variables['lon'][:]
+        sst = ncfile.variables['sea_surface_temperature'][:]
+        ncfile.close()
+
+        sst = np.squeeze(sst)
+        lon2d , lat2d = np.meshgrid(lon,lat)
+
+        fig1=plt.figure(figsize=(12, 5))
+        ax = plt.axes(projection=ccrs.PlateCarree())
+        toplt = sst
+        hdl = ax.pcolormesh(lon2d,lat2d,toplt, \
+	                transform = ccrs.PlateCarree(),cmap=cm.thermal)
+        ax.coastlines(resolution='110m', color ='k' )
+        ax.add_feature(cfeature.LAND, facecolor = '0.75')
+        ax.gridlines(draw_labels = True)
+        cb = plt.colorbar(hdl,ax=ax)
+        cb.set_label('SST [K]')
+
+        name_fig = file.rsplit(os.sep,1)[1].rsplit('.',2)[0]
+        plt.savefig(graphdir+os.sep+path_graph+os.sep+name_fig+'.png')
+        plt.show(block = False)
+        plt.close(fig1)
+        print('saving:',graphdir+os.sep+path_graph+os.sep+name_fig+'.png')
+        plt.close(fig1)
+
+def main():
+    #Region = sys.argv[1]
+    parser = argparse.ArgumentParser()
+    parser.add_argument('Region', help = 'Name of the region to inspec', type = str)
+    parser.add_argument('-o', '--output', help = 'Root directory were plots will be saved')
+    parser.add_argument('-i', '--input', help = 'Root directory were NetCDF are stored')
+
+    args = parser.parse_args()
+    print('arg.out:', args.output)
+
+    if args.output:
+        graphdir = args.output
+    else:
+        graphdir= '/home/cercache/users/cgonzale/snapshots'
+
+    if args.input:
+        datadir = args.input
+    else:
+        datadir= '/home/cercache/users/cgonzale/data/test'
+
+    
+    plot_snapshots_SST(args.Region, datadir = datadir , graphdir =  graphdir)
+
+if __name__ == '__main__':
+   main()
+
+
+#python3 plot_snapshots_SST.py Mozamb1 -o /home/cercache/users/cgonzale/graph_dir -i /home/cercache/users/cgonzale/datain
+
+
